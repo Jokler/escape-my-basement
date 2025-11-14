c2121f6c4fb37efcf3e1e310d888a9287beaed9e
Jokler <github@jokler.xyz>
2025-11-14 14:56:56 +0100

    Initial commit

---
+++ src/dev_tools.rs
@@ -0,0 +1,33 @@
+//! Development tools for the game. This plugin is only enabled in dev builds.
+
+use bevy::{
+    dev_tools::states::log_transitions,
+    input::common_conditions::{input_just_pressed, input_toggle_active},
+    prelude::*,
+};
+use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
+
+use crate::screens::Screen;
+
+pub(super) fn plugin(app: &mut App) {
+    // Log `Screen` state transitions.
+    app.add_systems(Update, log_transitions::<Screen>);
+
+    app.add_plugins((
+        EguiPlugin::default(),
+        WorldInspectorPlugin::new().run_if(input_toggle_active(false, TOGGLE_INSPECTOR_KEY)),
+    ));
+
+    // Toggle the debug overlay for UI.
+    app.add_systems(
+        Update,
+        toggle_debug_ui.run_if(input_just_pressed(TOGGLE_DEBUG_UI_KEY)),
+    );
+}
+
+const TOGGLE_INSPECTOR_KEY: KeyCode = KeyCode::Backquote;
+const TOGGLE_DEBUG_UI_KEY: KeyCode = KeyCode::KeyU;
+
+fn toggle_debug_ui(mut options: ResMut<UiDebugOptions>) {
+    options.toggle();
+}
